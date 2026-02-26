7d9c16298f41b6943154798869ba1e046bb5199c
Pravali Uppugunduri <upravali@amazon.com>
2026-02-26 18:10:35 +0000

    fixes to choose the right jar

--- scala-spark-sdk/src/main/scala-spark-3.5/software/amazon/sagemaker/featurestore/sparksdk/SparkRowEncoderAdaptor.scala
+++ scala-spark-sdk/src/main/scala-spark-3.5/software/amazon/sagemaker/featurestore/sparksdk/SparkRowEncoderAdaptor.scala
@@ -1,12 +1,11 @@
 package software.amazon.sagemaker.featurestore.sparksdk
 
-import org.apache.spark.sql.catalyst.encoders.RowEncoder
-import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.encoders.{ExpressionEncoder, RowEncoder}
 import org.apache.spark.sql.types.StructType
 
-object SparkRowEncoderAdaptor {
-  def encoderFor(schema: StructType): ExpressionEncoder[Row] = {
+object SparkRowEncoderAdaptor extends SparkRowEncoderAdaptorLike {
+  override def encoderFor(schema: StructType): ExpressionEncoder[Row] = {
     // For Spark 3.5+, RowEncoder.encoderFor(schema) is the API and returns an AgnosticEncoder or ExpressionEncoder depending on usage.
     // Fortunately casting to ExpressionEncoder[Row] or the implicit conversion is typically compatible, 
     // but the underlying method is `.encoderFor()`. To be safe, we just use RowEncoder.encoderFor
